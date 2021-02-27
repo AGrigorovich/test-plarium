4dd3648baa359c5c86b9943fefa45d12b6f1c62c
AGrigorovich <alexey.grigorovich@fluvius.co>
2021-02-27 12:40:05 +0200

    fixed and markup change colors component

--- src/components/ChangeColors/ChangeColors.js
+++ src/components/ChangeColors/ChangeColors.js
@@ -1,24 +1,40 @@
 import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-import './ChangeColors.css';
 import AppDropdown from "../AppDropdown/AppDropdown";
 import AppColorSquare from "../AppColorSquare/AppColorSquare";
 
-const renderInputsRanges = () => <di>Hello</di>
+import { clickable } from '../../helpers/makeElementClickable';
+
+import './ChangeColors.css';
+import {ComponentMouseDownHandler} from "../../helpers/ComponentMouseDownHandler";
+
+const renderInputsRanges = (changedMenuVisibility, changeColorRef) => <>
+		<div
+			onMouseDown={ ComponentMouseDownHandler(changeColorRef, () => changedMenuVisibility(false)) }
+		>
+			<span>Hello</span>
+		</div>
+	</>
 
 const ChangeColors = ({ value, onChange }) => {
 	const [isMenuOpen, changedMenuVisibility] = useState(false);
 	const changeColorRef = useRef(null);
+
 	return <div className="color-picker-rows">
-		<AppColorSquare value={value} />
+		<div
+			{...clickable(() => changedMenuVisibility(!isMenuOpen))}
+			className="change-color-color-square-wrapper"
+		>
+			<AppColorSquare value={value} />
+		</div>
 		{isMenuOpen &&
 		<div
 			ref={changeColorRef}
 			className="app-dropdown-wrapper"
 		>
 			<AppDropdown
-				renderPropsElements={() => renderInputsRanges()}
+				renderPropsElements={() => renderInputsRanges(changedMenuVisibility, changeColorRef)}
 			/>
 		</div>
 		}
