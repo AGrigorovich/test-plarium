1052a3cfe84bbdf6e4e81144fcf0f4f4a13ed7ac
AGrigorovich <alexey.grigorovich@fluvius.co>
2021-02-27 13:36:12 +0200

    fixed markup and created logic for changed colors from ranges

--- src/components/ChangeColors/ChangeColors.js
+++ src/components/ChangeColors/ChangeColors.js
@@ -1,44 +1,101 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import AppDropdown from "../AppDropdown/AppDropdown";
 import AppColorSquare from "../AppColorSquare/AppColorSquare";
 
-import { clickable } from '../../helpers/makeElementClickable';
-
-import './ChangeColors.css';
 import {ComponentMouseDownHandler} from "../../helpers/ComponentMouseDownHandler";
 
-const renderInputsRanges = (changedMenuVisibility, changeColorRef) => <>
-		<div
-			onMouseDown={ ComponentMouseDownHandler(changeColorRef, () => changedMenuVisibility(false)) }
-		>
-			<span>Hello</span>
-		</div>
-	</>
+import { clickable } from '../../helpers/makeElementClickable';
+import { colorRanges } from '../../constants/colorRanges';
+
+import './ChangeColors.css';
 
 const ChangeColors = ({ value, onChange }) => {
 	const [isMenuOpen, changedMenuVisibility] = useState(false);
+	const [currentColor, changedCurrentColor] = useState(false);
+	const [colorRedValue, changedColorRed] = useState(0);
+	const [colorGreenValue, changedColorGreen] = useState(0);
+	const [colorBlueValue, changedColorBlue] = useState(0);
 	const changeColorRef = useRef(null);
 
-	return <div className="color-picker-rows">
-		<div
-			{...clickable(() => changedMenuVisibility(!isMenuOpen))}
-			className="change-color-color-square-wrapper"
-		>
-			<AppColorSquare value={value} />
-		</div>
-		{isMenuOpen &&
-		<div
-			ref={changeColorRef}
-			className="app-dropdown-wrapper"
-		>
-			<AppDropdown
-				renderPropsElements={() => renderInputsRanges(changedMenuVisibility, changeColorRef)}
-			/>
-		</div>
+	const detectValue = color =>{
+		switch(color){
+			case 'red': return colorRedValue;
+			case 'green': return colorGreenValue;
+			case 'blue': return colorBlueValue;
+			default: return '';
 		}
-	</div>
+	}
+	const detectFunction = (value, color) => {
+		switch(color){
+			case 'red': return changedColorRed(value);
+			case 'green': return changedColorGreen(value);
+			case 'blue': return changedColorBlue(value);
+			default: return null;
+		}
+	}
+
+	const renderInputsRanges = (changedMenuVisibility, changeColorRef) => <>
+		<div
+			onMouseDown={ ComponentMouseDownHandler(changeColorRef, () => changedMenuVisibility(false)) }
+		>
+			<>
+				{colorRanges.map(({ id, name, value }) => (
+					<div
+						key={id}
+						className="input-range-item"
+					>
+						<span>{name}</span>
+						<input
+							type="range"
+							id={id}
+							name={name}
+							min={0}
+							max={255}
+							step={1}
+							value={detectValue(value)}
+							onChange={event => detectFunction(event.target.value, value)}
+						/>
+					</div>
+				))}
+			</>
+		</div>
+	</>
+
+	useEffect(
+		() => {
+			let hex = value.replace('#', '');
+
+			const redColor = parseInt(hex.substring(0, 2), 16);
+			const greenColor = parseInt(hex.substring(2, 4), 16);
+			const blueColor = parseInt(hex.substring(4, 6), 16);
+			changedColorRed(redColor);
+			changedColorGreen(greenColor);
+			changedColorBlue(blueColor);
+
+		}, [value]);
+
+	const detectCurrentColor = () => `rgba(${colorRedValue},${colorGreenValue},${colorBlueValue})`;
+
+	return(
+		<div className="color-picker-rows">
+			<div
+				{...clickable(() => changedMenuVisibility(!isMenuOpen))}
+				className="change-color-color-square-wrapper"
+			>
+				<AppColorSquare value={detectCurrentColor()} />
+			</div>
+			{isMenuOpen &&
+			<div
+				ref={changeColorRef}
+				className="app-dropdown-wrapper"
+			>
+				<AppDropdown
+					renderPropsElements={() => renderInputsRanges(changedMenuVisibility, changeColorRef)}
+				/>
+			</div>}
+		</div>)
 };
 
 ChangeColors.propTypes = {
---
+++ src/constants/colorRanges.js
@@ -0,0 +1,5 @@
+export const colorRanges = [
+	{ id: 0, name: 'R', value: 'red' },
+	{ id: 1, name: 'G', value: 'green' },
+	{ id: 2, name: 'B', value: 'blue' },
+];
