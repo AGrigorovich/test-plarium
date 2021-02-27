4b102063d424bf33a6a886f02376ea46499f28f1
AGrigorovich <alexey.grigorovich@fluvius.co>
2021-02-27 12:11:25 +0200

    refactoring logic for select basic colors

--- src/components/SelectBasicColors/SelectBasicColors.js
+++ src/components/SelectBasicColors/SelectBasicColors.js
@@ -1,25 +1,51 @@
-import React, { useState } from 'react';
+import React, {useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 
-import SelectColorPicker from '../SelectColorPicker/SelectColorPicker';
+import AppDropdown from '../SelectColorPicker/AppDropdown';
 
 import { clickable } from '../../helpers/makeElementClickable';
+import { ComponentMouseDownHandler } from "../../helpers/ComponentMouseDownHandler";
 
 import './SelectBasicColors.css';
 
+const renderColors = (colors, handleSelectColor, changedSelectVisibility, selectRef) => <>
+	{colors.map(({ id, name, value }) => (
+		// we can create component for items, but it's not necessary now
+		<div
+			key={id}
+			className="select-color-picker-item"
+			{...clickable(() => handleSelectColor(value))}
+			onMouseDown={ComponentMouseDownHandler(selectRef, () => changedSelectVisibility(false))}
+		>
+			<span>{name}</span>
+			<div
+				className="select-color-picker-color-square"
+				style={{backgroundColor: value}}
+			/>
+		</div>)
+	)}</>
+
 const SelectBasicColors = ({ colors, onChange }) => {
 	const [isSelectOpen, changedSelectVisibility] = useState(false);
+	const selectRef = useRef(null);
+	const handleSelectColor = colorValue => {
+		onChange(colorValue);
+	}
 	return <div className="color-picker-rows">
 		<div
 			className="arrow-down"
 			{...clickable(() => changedSelectVisibility(!isSelectOpen))}
 		/>
 		{isSelectOpen &&
-		<SelectColorPicker
-			colors={colors}
-			onChange={onChange}
-			changedSelectVisibility={changedSelectVisibility}
-		/>}
+			<div
+				ref={selectRef}
+				className="app-dropdown-wrapper"
+			>
+				<AppDropdown
+					renderPropsElements={() => renderColors(colors, handleSelectColor, changedSelectVisibility, selectRef)}
+				/>
+			</div>
+	}
 	</div>
 };
 
---
+++ src/components/SelectColorPicker/AppDropdown.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './SelectColorPicker.css';
+
+const AppDropdown = ({ renderPropsElements }) =>
+		<>
+			<div className="arrow-up"/>
+			<div
+				className="select-color-picker"
+			>
+				{renderPropsElements()}
+			</div>
+		</>;
+
+AppDropdown.propTypes = {
+	renderPropsElements: PropTypes.func.isRequired,
+};
+
+export default AppDropdown;
